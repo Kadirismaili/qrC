c7ed077b31a27bfbe5f2fb537c703c3496cc428e
Fly <kiwipromm@gmail.com>
2022-12-12 15:22:34 +0300

    first commit

---
+++ screens/Home.js
@@ -0,0 +1,25 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+import React from 'react';
+
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home (){
+    const navigation =useNavigation();
+
+    return(
+        <View style={StyleSheet.container}>
+            <Button title='Scan' onPress={()=> navigation.navigate('Scanner')} />
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+        backroundcolor:'#fff',
+        alignItems:'center',
+        justifyContent:'center'
+        
+    }
+})
